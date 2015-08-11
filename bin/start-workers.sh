353428a3902b659419b15f3587809941c2256c98
Petr Janda <petrjandacz@gmail.com>
2015-08-11 18:48:41 +0100

    dont start gluster client

--- bin/start-workers.sh
+++ bin/start-workers.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-fleetctl load services/glusterfs-client@{1,2}.service
 fleetctl load services/spark-worker@{1,2}.service
-
-fleetctl start services/glusterfs-client@{1,2}.service
 fleetctl start services/spark-worker@{1,2}.service
--- bin/stop-workers.sh
+++ bin/stop-workers.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 fleetctl stop services/spark-worker@{1,2}.service
-fleetctl stop services/glusterfs-client@{1,2}.service
 
 fleetctl destroy services/spark-worker@{1,2}.service
-fleetctl destroy services/glusterfs-client@{1,2}.service
