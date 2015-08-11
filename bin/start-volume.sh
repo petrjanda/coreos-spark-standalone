d00359678fd9ecebc11014b96adcba2a9a759cdd
Petr Janda <petrjandacz@gmail.com>
2015-08-11 18:48:15 +0100

    remove gluster client

--- bin/start-volume.sh
+++ bin/start-volume.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-GLUSTER_IPS=$(/usr/bin/etcdctl ls /services/glusterfs | xargs -I {} /usr/bin/etcdctl get {})
+export VOL_NAME=$1
+export GLUSTER_IPS=$(/usr/bin/etcdctl ls /services/glusterfs | xargs -I {} /usr/bin/etcdctl get {} | xargs -I {} echo "{}:/media/gfs/$VOL_NAME" | tr '\n' ' ')
 
+echo "--> $VOL_NAME replica 2 $GLUSTER_IPS"
+/usr/bin/docker exec -it glusterfs-1 gluster volume create $VOL_NAME replica 2 $GLUSTER_IPS
+/usr/bin/docker exec -it glusterfs-1 gluster volume start $VOL_NAME
+/usr/bin/docker exec -it glusterfs-1 gluster volume info $VOL_NAME
